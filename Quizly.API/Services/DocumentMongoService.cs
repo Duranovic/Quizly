97599219791c6623a796cb093a73c4a4f352c149
Velid Duranovic <velid_provjera@outlook.com>
2020-12-30 17:51:07 +0100

    [API]  *{Collection name - connection string settings}

--- Quizly.API/Services/DocumentMongoService.cs
+++ Quizly.API/Services/DocumentMongoService.cs
@@ -13,7 +13,7 @@ namespace Quizly.API.Services
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
 
-            _documents = database.GetCollection<Document>("Documents");
+            _documents = database.GetCollection<Document>(settings.DocumentsCollectionName);
         }
 
         public IEnumerable<Document> Get()
