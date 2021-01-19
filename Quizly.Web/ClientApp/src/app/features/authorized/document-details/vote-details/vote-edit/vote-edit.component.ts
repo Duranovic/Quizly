b20926086976e8114049e2a51aab1faa7138f3b6
Velid Duranovic <velid_provjera@outlook.com>
2021-01-19 23:25:26 +0100

    [Client Web App] * {Worked on vote image uploader}

--- Quizly.Web/ClientApp/src/app/features/authorized/document-details/vote-details/vote-edit/vote-edit.component.ts
+++ Quizly.Web/ClientApp/src/app/features/authorized/document-details/vote-details/vote-edit/vote-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-vote-edit',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteEditComponent implements OnInit {
 
-  constructor() { }
+  voteEditFormGroup: FormGroup;
+  imagePath;
+  url;
+
+  constructor() { 
+    this.voteEditFormGroup = new FormGroup({
+      name: new FormControl(""),
+      description: new FormControl("This is a description!"),
+      image: new FormControl("")
+    });    
+  }
 
   ngOnInit() {
+  }
+  submitForm(){
+    
+  }
+
+  onFileChanged(event){
+    const files = event.target.files;
+    if (files.length === 0)
+        return;
+
+    const mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+        return;
+    }
+
+    const reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+        this.url = reader.result; 
+    }
   }
 
 }
