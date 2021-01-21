8cdc9d031452a96f22b5c2f5f411d447d7ab0edc
Velid Duranovic <velid_provjera@outlook.com>
2021-01-21 23:15:04 +0100

    [Client Web App] * {Document Services refactoring}

--- Quizly.Web/ClientApp/src/app/core/services/documents-api.service.ts
+++ Quizly.Web/ClientApp/src/app/core/services/documents-api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { Config } from 'src/app/shared/models/config';
 import {Document} from 'src/app/shared/models/document';
 
 
@@ -12,41 +13,38 @@ export class DocumentsApiService {
   documents: Document[];
   recentDocuments: Document[];
   pinnedDocuments: Document[];
+  globalConfig: Config;
   constructor(readonly http: HttpClient) {
 
    }
    getDocumentById(id:string){
-    return this.http.get(`http://localhost:65189/api/documents/GetDocumentById?id=${id}`);
+    return this.http.get(`${Config.baseUrl}/api/documents/GetDocumentById?id=${id}`);
    }
     getDocuments (){
-      return this.http.get("http://localhost:65189/api/documents/get").pipe(
+      return this.http.get(`${Config.baseUrl}/api/documents/get`).pipe(
         tap((data:Document[])=>{this.documents = data})
       );
     }
     getRecentDocuments (){
-      return this.http.get("http://localhost:65189/api/documents/getRecent").pipe(
+      return this.http.get(`${Config.baseUrl}/api/documents/getRecent`).pipe(
         tap((data:Document[])=>{this.recentDocuments = data})
       );
     }
     getPinnedDocuments (){
-      return this.http.get("http://localhost:65189/api/documents/getPinned").pipe(
+      return this.http.get(`${Config.baseUrl}/api/documents/getPinned`).pipe(
         tap((data:Document[])=>{this.pinnedDocuments = data})
       );
     }
     pinDocument(id:string){
-      return this.http.patch(`http://localhost:65189/api/documents/pinDocument/${id}`, true);
+      return this.http.patch(`${Config.baseUrl}/api/documents/pinDocument/${id}`, true);
     }
     createDocument(titleAttr: string, typeAttr: string){
       let newDocument: Document = {
         title: titleAttr,
-        id: 2,
         dateTime: new Date(),
         pinned : false,
         type: typeAttr
       };
-      let document;
-      return this.http.put("http://localhost:65189/api/documents/create", newDocument).pipe(
-        tap(data=>{document = data; console.log("DATA: " + data)})
-      );
+      return this.http.put(`${Config.baseUrl}/api/documents/create`, newDocument);
     }
 }
