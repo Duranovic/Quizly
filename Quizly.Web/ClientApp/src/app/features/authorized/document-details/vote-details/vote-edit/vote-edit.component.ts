5b2ac2201b2105f923b82b5f4987435e4f583567
Velid Duranovic <velid_provjera@outlook.com>
2021-01-22 22:32:05 +0100

    [Client Web App] * {Vote edit component - bind document details}

--- Quizly.Web/ClientApp/src/app/features/authorized/document-details/vote-details/vote-edit/vote-edit.component.ts
+++ Quizly.Web/ClientApp/src/app/features/authorized/document-details/vote-details/vote-edit/vote-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { DocumentsApiService } from 'src/app/core/services/documents-api.service';
+import {Document} from '../../../../../shared/models/document';
 
 @Component({
   selector: 'app-vote-edit',
@@ -11,16 +14,14 @@ export class VoteEditComponent implements OnInit {
   voteEditFormGroup: FormGroup;
   imagePath;
   url;
+  document: Document;
 
-  constructor() { 
-    this.voteEditFormGroup = new FormGroup({
-      name: new FormControl(""),
-      description: new FormControl("This is a description!"),
-      image: new FormControl("")
-    });    
+  constructor(private documentService: DocumentsApiService, private route: ActivatedRoute) { 
+    this.document = new Document(); 
   }
 
   ngOnInit() {
+    this.getDocumentDetails(this.route.snapshot._urlSegment.segments[2].path);      
   }
   submitForm(){
     
@@ -43,5 +44,16 @@ export class VoteEditComponent implements OnInit {
         this.url = reader.result; 
     }
   }
-
+  getDocumentDetails(id:string){
+    this.documentService.getDocumentById(id).subscribe({
+      next: (data: Document)=>{
+        this.document = data;
+        this.voteEditFormGroup = new FormGroup({
+          name: new FormControl(this.document.title),
+          description: new FormControl((this.document.desctiption)?this.document.desctiption:"Vote description"),
+          image: new FormControl("")
+        }); 
+      }
+    })
+  }
 }
--- Quizly.Web/ClientApp/src/app/shared/models/document.ts
+++ Quizly.Web/ClientApp/src/app/shared/models/document.ts
@@ -2,6 +2,7 @@ export class Document{
   _id?:string;
   title: string;
   type: string;
+  desctiption?: string;
   pinned?: boolean;
-  dateTime: Date
+  dateTime: Date;
 }
