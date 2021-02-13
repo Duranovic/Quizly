e42b07475ed3febea1e71277f5e9ba1c9d511b3a
Velid Duranovic <velid_work@outlook.com>
2021-02-13 23:27:00 +0100

    [Web API]
    Added async to the API

--- Quizly.API/Services/IDocumentService.cs
+++ Quizly.API/Services/IDocumentService.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Quizly.Model;
 
 namespace Quizly.API.Services
 {
     public interface IDocumentService
     {
-        public IEnumerable<Document> Get();
-        public Document GetDocumentById(string id);
-        public IEnumerable<Document> GetRecent();
-        public IEnumerable<Document> GetPinned();
-        public Document PinDocument(string id);
-        public IEnumerable<Document> PinManyDocuments(string[] ids, bool flag);
-        public Document CreateDocument(Document document);
-        public Document Delete(string id);
-        public IEnumerable<Document> DeleteMany(string[] ids);
+        public Task<IEnumerable<Document>> Get();
+        public Task<Document> GetDocumentById(string id);
+        public Task<IEnumerable<Document>> GetRecent();
+        public Task<IEnumerable<Document>> GetPinned();
+        public Task<Document> PinDocument(string id);
+        public Task<IEnumerable<Document>> PinManyDocuments(string[] ids, bool flag);
+        public Task<Document> CreateDocument(Document document);
+        public Task<Document> Delete(string id);
+        public Task<IEnumerable<Document>> DeleteMany(string[] ids);
     }
 }
